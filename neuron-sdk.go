fb9c819bcf2e2dd44753bad92087bc5a87f78096
Craig Woodland <craig.woodland@neuron.world>
2025-06-06 19:04:04 +0100

    4dskyedge-sdkv2

--- neuron-sdk.go
+++ neuron-sdk.go
@@ -2,6 +2,7 @@ package sdk
 
 import (
 	"context"
+	"encoding/base64"
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
@@ -14,6 +15,7 @@ import (
 	commonlib "github.com/NeuronInnovations/neuron-go-hedera-sdk/common-lib"
 	flags "github.com/NeuronInnovations/neuron-go-hedera-sdk/common-lib"
 	neuronbuffers "github.com/NeuronInnovations/neuron-go-hedera-sdk/common-lib"
+	hedera_helper "github.com/NeuronInnovations/neuron-go-hedera-sdk/hedera"
 	sdk "github.com/NeuronInnovations/neuron-go-hedera-sdkv2/sdk"
 	sdktypes "github.com/NeuronInnovations/neuron-go-hedera-sdkv2/types"
 	p2p "github.com/NeuronInnovations/neuron-go-tunnel-sdkv2/p2p"
@@ -26,6 +28,8 @@ import (
 	"github.com/libp2p/go-libp2p/core/peer"
 	"github.com/libp2p/go-libp2p/core/protocol"
 	"github.com/multiformats/go-multiaddr"
+	"github.com/umahmood/haversine"
+	"golang.org/x/time/rate"
 )
 
 var outboundChannel = make(chan []byte)
@@ -53,7 +57,7 @@ func LaunchSDK(
 	version string,
 	protocol protocol.ID,
 	keyAndLocationConfigurator func(envIsReady chan bool, envFile string) error,
-	buyerCase func(ctx context.Context, p2pHost host.Host, buffers *neuronbuffers.NodeBuffers),
+	buyerCase func(ctx context.Context, p2pHost host.Host, receivedData chan []byte),
 	buyerCaseTopicListener func(topicMessage hedera.TopicMessage),
 	sellerCase func(ctx context.Context, p2pHost host.Host, buffers *neuronbuffers.NodeBuffers),
 	sellerCaseTopicListener func(topicMessage hedera.TopicMessage),
@@ -81,7 +85,7 @@ func LaunchSDK(
 	go func() {
 		switch *flags.BuyerOrSellerFlag {
 		case "buyer":
-			initBuyer(ctx, p2pHost, protocol, address, peerId)
+			initBuyer(ctx, p2pHost, protocol, address, peerId, buyerCase)
 		case "seller":
 			initSeller(ctx, p2pHost)
 		default:
@@ -103,7 +107,7 @@ func LaunchSDK(
 	os.Exit(0)
 }
 
-func initBuyer(ctx context.Context, p2pHost host.Host, protocol protocol.ID, address string, peerId peer.ID) {
+func initBuyer(ctx context.Context, p2pHost host.Host, protocol protocol.ID, address string, peerId peer.ID, buyerCase func(ctx context.Context, p2pHost host.Host, receivedData chan []byte)) {
 	buffers := p2p.NewNodeBuffers()
 
 	addressMultiaddr, err := multiaddr.NewMultiaddr(address)
@@ -152,14 +156,132 @@ func initBuyer(ctx context.Context, p2pHost host.Host, protocol protocol.ID, add
 
 	go p2p.CreateStreamHandler(p2pHost, buffers, protocol, receivedData)
 
+	go buyerCase(ctx, p2pHost, receivedData)
+
+	if *flags.ListOfSellersSourceFlag == "env" {
+		var listOfSellersEnvList = os.Getenv("list_of_sellers")
+
+		if listOfSellersEnvList == "" {
+			log.Println("list_of_sellers is empty")
+			return
+		}
+
+		fmt.Println("Loading sellers from env")
+	} else {
+		fmt.Println("Loading sellers from explorer")
+
+		sellerList := make(map[string]bool)
+
+		go func() {
+			var limiter = rate.NewLimiter(5, 1)
+
+			for {
+				devices, err := hedera_helper.GetAllDevicesFromExplorer()
+
+				if err != nil {
+					log.Println("💀  GetAllPeers error: ", err)
+				}
+
+				log.Println("🔎  got ", len(devices), " devices from the explorer")
+
+				center := haversine.Coord{
+					Lat: commonlib.MyLocation.Latitude,
+					Lon: commonlib.MyLocation.Longitude,
+				}
+
+				for _, device := range devices {
+					publicKey, publicKeyOk := device["publickey"].(string)
+					devicerole, deviceRoleOk := device["devicerole"].(float64)
+
+					// Check if the device has a valid public key and device role
+					if !publicKeyOk || !deviceRoleOk {
+						continue
+					}
+
+					// Check if the device is a seller
+					if devicerole != 0 {
+						continue
+					}
+
+					stdout := device["topic_stdout"].(string)
+					stdoutTyped, _ := hedera.TopicIDFromString(stdout)
+
+					limiter.Wait(context.Background())
+
+					m, lastMessageError := hedera_helper.GetLastMessageFromTopic(stdoutTyped)
+
+					if lastMessageError != nil {
+						continue
+					}
+
+					if m.Timestamp.IsZero() || m.Timestamp.Before(time.Now().Add(-10*time.Minute)) {
+						continue
+					}
+
+					hpub, err := hedera.PublicKeyFromString(publicKey)
+
+					if err != nil {
+						log.Println("💀  hedera.PublicKeyFromString error: ", err)
+						continue
+					}
+
+					publicKey = hpub.StringRaw()
+					heartbeatMessage := new(commonlib.NeuronHeartBeatMsg)
+					base64Decoded, _ := base64.StdEncoding.DecodeString(m.Message)
+					err = json.Unmarshal([]byte(base64Decoded), &heartbeatMessage)
+
+					if err != nil {
+						continue
+					}
+
+					// Check if the radius flag is set, if set then check if the seller is in the radius
+					if *flags.RadiusFlag > 0 {
+						radius := flags.RadiusFlag
+
+						// calculate the distance using the haversine formula
+						farPoint := haversine.Coord{
+							Lat: heartbeatMessage.Location.Latitude,
+							Lon: heartbeatMessage.Location.Longitude,
+						}
+
+						_, distanceKm := haversine.Distance(center, farPoint)
+
+						// check if the distance is less than the radius
+						if int(distanceKm) > *radius {
+							continue
+						}
+					}
+
+					fmt.Println("Found valid seller", publicKey)
+
+					if _, ok := sellerList[publicKey]; !ok {
+						fmt.Println("Adding seller to list", publicKey)
+
+						sellerList[publicKey] = true
+
+						previousSharedAccountID := ""
+
+						_, err := sdk.ServiceRequest(ctx, string(protocol), buyerHederaConfig.Multiaddr, publicKey, &previousSharedAccountID)
+
+						if err != nil {
+							fmt.Println("Error sending service request", err)
+						}
+					}
+				}
+
+				time.Sleep(120 * time.Second)
+			}
+		}()
+	}
+
 	// Simple message handling loop
 	for {
 		select {
 		case <-ctx.Done():
 			fmt.Println("Shutting down buyer...")
 			return
-		case msg := <-receivedData:
-			fmt.Println("Received message from inbound channel", string(msg))
+		// case msg := <-receivedData:
+		// 	fmt.Println("Received message from inbound channel", len(msg))
 		default:
 			// Add a small sleep to prevent CPU spinning
 			time.Sleep(100 * time.Millisecond)
